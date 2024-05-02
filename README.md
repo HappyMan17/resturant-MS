08614b8528f7770f04f231ba0bbfb4fbd409403c
HappyMan17 <sebasgrisales16@gmail.com>
2024-05-01 21:44:00 -0500

    feat: environment variables added to the project and requirements file created.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # resturant-MS
 Order and inventory management system for restaurants.
+
+To save all used python dependencies:
+```bash
+  py -m pip freeze > requirements.txt
+```
+
+To install all python dependencies use:
+```bash
+  py -m pip install -r requirements.txt
+```
+
+To install all Angular dependencies use:
+```bash
+  npm intall
+```
--- back/main.py
+++ back/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from config.env import env
 
 # Documentation: https://fastapi.tiangolo.com/tutorial/first-steps/
 
@@ -10,6 +11,13 @@ async def root():
   return {
     "ok": True,
     "message": "Hello World",
+  }
+
+@app.get("/port")
+async def port():
+  return {
+    "ok": True,
+    "message": f"running on port: { env['PORT'] }",
   }
 
 @app.get("/items/{item_id}")
